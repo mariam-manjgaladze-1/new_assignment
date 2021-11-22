15719e6477a6627616dd280025ed246ddddd9b51
Your Name <you@example.com>
2021-11-22 22:02:53 +0400

    i guess this is for specifying updates.... bruh.

--- app/src/main/java/com/example/newassignment/MainActivity.kt
+++ app/src/main/java/com/example/newassignment/MainActivity.kt
@@ -21,10 +21,13 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var button7:Button
     private lateinit var button8:Button
     private lateinit var button9:Button
+    private lateinit var text: TextView
     private var player = 1
     private var p1 = ArrayList<Int>()
     private var p2 = ArrayList<Int>()
     private var clicks = 0
+    private lateinit var start:Button
+    private var start_b = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,6 +36,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     }
 
         private fun init() {
+            text = findViewById<TextView>(R.id.text)
             button1 = findViewById<Button>(R.id.button1)
             button2 = findViewById<Button>(R.id.button2)
             button3 = findViewById<Button>(R.id.button3)
@@ -42,6 +46,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             button7 = findViewById<Button>(R.id.button7)
             button8 = findViewById<Button>(R.id.button8)
             button9 = findViewById<Button>(R.id.button9)
+            start = findViewById<Button>(R.id.start)
 
             button1.setOnClickListener(this)
             button2.setOnClickListener(this)
@@ -52,29 +57,45 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             button7.setOnClickListener(this)
             button8.setOnClickListener(this)
             button9.setOnClickListener(this)
+            start.setOnClickListener(this)
         }
 
 
     override fun onClick(clickedView: View?) {
-        if(clickedView is Button ){
-            var bNum = 0
-            when (clickedView.id){
-                R.id.button1 -> bNum = 1
-                R.id.button2 -> bNum = 2
-                R.id.button3 -> bNum = 3
-                R.id.button4 -> bNum = 4
-                R.id.button5 -> bNum = 5
-                R.id.button6 -> bNum = 6
-                R.id.button7 -> bNum = 7
-                R.id.button8 -> bNum = 8
-                R.id.button9 -> bNum = 9
+            if (clickedView is Button) {
+                when (clickedView.id) {
+                    R.id.start -> start_b = 1
+                }
+                if (start_b == 1) {
+                    button1.setBackgroundColor(Color.WHITE)
+                    button2.setBackgroundColor(Color.WHITE)
+                    button3.setBackgroundColor(Color.WHITE)
+                    button4.setBackgroundColor(Color.WHITE)
+                    button5.setBackgroundColor(Color.WHITE)
+                    button6.setBackgroundColor(Color.WHITE)
+                    button7.setBackgroundColor(Color.WHITE)
+                    button8.setBackgroundColor(Color.WHITE)
+                    button9.setBackgroundColor(Color.WHITE)
 
+                    var bNum = 0
+                    when (clickedView.id) {
+                        R.id.button1 -> bNum = 1
+                        R.id.button2 -> bNum = 2
+                        R.id.button3 -> bNum = 3
+                        R.id.button4 -> bNum = 4
+                        R.id.button5 -> bNum = 5
+                        R.id.button6 -> bNum = 6
+                        R.id.button7 -> bNum = 7
+                        R.id.button8 -> bNum = 8
+                        R.id.button9 -> bNum = 9
+
+                    }
+                    if (bNum != 0) {
+                        game(clickedView, bNum)
+                    }
+                }
             }
-            if (bNum != 0)
-            {
-                game(clickedView, bNum)
-            }
-        }
+
     }
 
     private fun game(clickedView: Button, bNum: Int) {
@@ -152,19 +173,60 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             win = 2
         }
 
+        fun restart(){
+            p1.clear()
+            p2.clear()
+            win = 0
+            player = 1
+            clicks = 0
+            start_b = 0
+
+            button1.text = ""
+            button2.text = ""
+            button3.text = ""
+            button4.text = ""
+            button5.text = ""
+            button6.text = ""
+            button7.text = ""
+            button8.text = ""
+            button9.text = ""
+
+            button1.setBackgroundColor(Color.BLACK)
+            button2.setBackgroundColor(Color.BLACK)
+            button3.setBackgroundColor(Color.BLACK)
+            button4.setBackgroundColor(Color.BLACK)
+            button5.setBackgroundColor(Color.BLACK)
+            button6.setBackgroundColor(Color.BLACK)
+            button7.setBackgroundColor(Color.BLACK)
+            button8.setBackgroundColor(Color.BLACK)
+            button9.setBackgroundColor(Color.BLACK)
+
+            button1.isEnabled = true
+            button2.isEnabled = true
+            button3.isEnabled = true
+            button4.isEnabled = true
+            button5.isEnabled = true
+            button6.isEnabled = true
+            button7.isEnabled = true
+            button8.isEnabled = true
+            button9.isEnabled = true
+
+        }
+
         //gamocxadeba
         if (win == 1) {
-            Toast.makeText(this, "player X won", Toast.LENGTH_SHORT).show()
-            finish()
+            text.text = "player X won"
+            restart()
         }
         else if (win == 2){
-            Toast.makeText(this, "player O won", Toast.LENGTH_SHORT).show()
-            finish()
+            text.text = "player O won"
+            restart()
         }
         else if (clicks == 9){
-            Toast.makeText(this, "draw", Toast.LENGTH_SHORT).show()
-            finish()
+            text.text = "Draw"
+            restart()
         }
+
     }
 
 }
